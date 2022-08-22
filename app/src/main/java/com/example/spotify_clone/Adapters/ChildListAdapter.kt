090c8b2fdd2f7b514a16a523081130a01ceaf6da
vickatGit <gangishettyvikas@gmail.com>
2022-08-22 17:40:05 +0530

    now child adapter can handle 2 views

--- app/src/main/java/com/example/spotify_clone/Adapters/ChildListAdapter.kt
+++ app/src/main/java/com/example/spotify_clone/Adapters/ChildListAdapter.kt
@@ -8,18 +8,34 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.adamratzman.spotify.utils.Locale
 import com.bumptech.glide.Glide
 import com.example.spotify_clone.Models.ApiRelatedModels.Thumbnail
 import com.example.spotify_clone.R
 
-class ChildListAdapter(val allThumbs: List<Thumbnail>, val requireActivity: FragmentActivity) : RecyclerView.Adapter<ChildListAdapter.ArtistHolder>() {
+class ChildListAdapter(val allThumbs: List<Thumbnail>, val requireActivity: FragmentActivity) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     var thumbs=ArrayList<Thumbnail>(1)
     private lateinit var adapter: ChildListAdapter
     lateinit var recycler:RecyclerView
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtistHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         thumbs.addAll(allThumbs)
-        val view=LayoutInflater.from(parent.context).inflate(R.layout.artist_layout,parent,false)
-        return ArtistHolder(view)
+        val view:View
+        if(viewType==0) {
+
+            view= LayoutInflater.from(parent.context).inflate(R.layout.generic_thumb_layout, parent, false)
+            return GenricThumbHolder(view)
+
+        }else{
+            view= LayoutInflater.from(parent.context).inflate(R.layout.artist_layout, parent, false)
+            return ArtistHolder(view)
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if(allThumbs.get(position).type.equals("artist",true))
+            return 1
+        else
+            return 0
     }
 
     override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
@@ -27,23 +43,21 @@ class ChildListAdapter(val allThumbs: List<Thumbnail>, val requireActivity: Frag
         this.recycler=recyclerView
     }
 
-    override fun onBindViewHolder(holder: ArtistHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val thumb=allThumbs.get(position)
-        Log.d("TAG", "onBindViewHolder: allThumbs $thumb")
+        if(holder.itemViewType==1) {
+            var holder=holder as ArtistHolder
+            Log.d("TAG", "onBindViewHolder: allThumbs $thumb")
 
-        Glide.with(holder.artistProfile.context).load(thumb.imageUrl).into(holder.artistProfile)
-        Log.d("TAG", "onBindViewHolder: "+thumb.name)
-        holder.artistName.text=thumb.name
-//        thumb.observer.observe(requireActivity, Observer {
-//            allThumbs.clear()
-//            allThumbs.addAll(it!!)
-//            Log.d("TAG", "onBindViewHolder: $it")
-//
-//            recycler.post {
-//                recycler.stopScroll()
-//                adapter.notifyDataSetChanged()
-//            }
-//        })
+            Glide.with(holder.artistProfile.context).load(thumb.imageUrl).into(holder.artistProfile)
+            Log.d("TAG", "onBindViewHolder: " + thumb.name)
+            holder.artistName.text = thumb.name
+        }else{
+            var holder=holder as GenricThumbHolder
+            Glide.with(holder.coverImage.context).load(thumb.imageUrl).into(holder.coverImage)
+            Log.d("TAG", "onBindViewHolder: " + thumb.name)
+            holder.artistsName.text = thumb.name
+        }
     }
 
 
@@ -68,6 +82,10 @@ class ChildListAdapter(val allThumbs: List<Thumbnail>, val requireActivity: Frag
         var artistProfile:ImageView=itemView.findViewById(R.id.artist_profile)
         var artistName:TextView=itemView.findViewById(R.id.artist_name)
 
+    }
+    inner class GenricThumbHolder(view: View) : RecyclerView.ViewHolder(view){
+        var coverImage:ImageView=view.findViewById(R.id.cover_image)
+        var artistsName:TextView=view.findViewById(R.id.artist_names)
 
     }
 
