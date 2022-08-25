d600e6be5452e9543c8fbd6e81feaaafedf91e2c
vickatGit <gangishettyvikas@gmail.com>
2022-08-25 12:31:34 +0530

    solving ide problem

--- app/src/main/java/com/example/spotify_clone/Adapters/SearchSongAdapter.kt
+++ app/src/main/java/com/example/spotify_clone/Adapters/SearchSongAdapter.kt
@@ -6,12 +6,11 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.adamratzman.spotify.models.Track
-import com.bumptech.glide.Glide
 import com.example.spotify_clone.R
+import com.example.spotify_clone.Models.ApiRelatedModels.Thumbnail
 
 
-class SearchSongAdapter(val searchedTracks: ArrayList<Track>) : RecyclerView.Adapter<SearchSongAdapter.SearchResultViewHolder>() {
+class SearchSongAdapter(val searchedTracks: kotlin.collections.ArrayList<Thumbnail>) : RecyclerView.Adapter<SearchSongAdapter.SearchResultViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchResultViewHolder {
         val view=LayoutInflater.from(parent.context).inflate(R.layout.track_layout,parent,false)
         return SearchResultViewHolder(view)
@@ -19,8 +18,8 @@ class SearchSongAdapter(val searchedTracks: ArrayList<Track>) : RecyclerView.Ada
 
     override fun onBindViewHolder(holder: SearchResultViewHolder, position: Int) {
         val track=searchedTracks.get(position)
-        Glide.with(holder.songCover).load(track?.album.images.get(track?.album?.images.size-1).url).into(holder.songCover)
-        holder.songName.text=track.name
+//        Glide.with(holder.songCover).load(track?.album.images.get(track?.album?.images.size-1).url).into(holder.songCover)
+//        holder.songName.text=track.name
 //        holder.songArtist.text=track.artists.get(0).name
     }
 
@@ -30,7 +29,7 @@ class SearchSongAdapter(val searchedTracks: ArrayList<Track>) : RecyclerView.Ada
     inner class SearchResultViewHolder(view: View) : RecyclerView.ViewHolder(view){
         val songCover: ImageView=view.findViewById(R.id.song_thumbnail)
         val songName:TextView=view.findViewById(R.id.song_name)
-//        val songArtist:TextView=view.findViewById(R.id.song_artist)
+        val songArtist:TextView=view.findViewById(R.id.artists)
     }
 
 }
